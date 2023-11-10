59b858419f986ae8b20c8e104fe0b00f3d205ae6
foenich <111463200+foenich@users.noreply.github.com>
2023-11-11 00:54:00 +0100

    Update IOBoardController.cpp
    Increase calculation accuracy. Part tolerances and imprecise calculation could add up to a wrong board-ID

--- src/IOBoardController.cpp
+++ src/IOBoardController.cpp
@@ -11,9 +11,8 @@ IOBoardController::IOBoardController(int cT)
 
     if (controllerType == CONTROLLER_16_8_1)
     {
-        // Read bordID. The read value is between 60 and 940.
-        boardId = 16 - ((int)((analogRead(28) + 30) / 60));
-
+        // Read bordID. Ideal value at 10bit resolution: (DIP+1)*1023*2/35 -> 58.46 to 935.3
+        boardId = 16 - ((int)((analogRead(28) + 29.32) / 58.46));
         _eventDispatcher->setRS485ModePin(2);
         _eventDispatcher->setBoard(boardId);
         _eventDispatcher->setCrossLinkSerial(Serial1);
